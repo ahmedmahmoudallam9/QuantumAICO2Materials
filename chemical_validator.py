515bb68ca49ab21273d69da8f48711b6d0518613
ahmedmahmoudallam9 <ahmedmahmoudallam9@gmail.com>
2025-12-09 21:57:31 +0200

    Update chemical_validator.py

--- chemical_validator.py
+++ chemical_validator.py
@@ -24,8 +24,7 @@ class ChemicalValidator:
         return np.mean(np.log(diffs)) if len(diffs) > 0 else -1.0
 
     def predict_performance(self, recipe, quantum_score, temp_c, acid_time_min):
-        # 1. Surface Area (The Biochar Engine)
-        optimal_temp_area = 580 # Refined optimal temp
+        optimal_temp_area = 580 
         temp_factor = np.exp(-((temp_c - optimal_temp_area)**2) / 3000)
         
         acid_factor = min(1.0, (acid_time_min / 120.0)) * ACTIVATION_PHYSICS["SILICA_REMOVAL_EFFICIENCY"]
@@ -34,34 +33,27 @@ class ChemicalValidator:
         generated_surface_area = ACTIVATION_PHYSICS["BASE_SURFACE_AREA"] + \
                                  (ACTIVATION_PHYSICS["MAX_THEORETICAL_AREA"] * temp_factor * acid_factor * 0.55)
 
-        # 2. Surface Chemistry (Biochar Pyrones)
         optimal_temp_chem = 520
         chem_factor = np.exp(-((temp_c - optimal_temp_chem)**2) / 7000)
         
-        # 3. THE GELATIN AMINE BOOST (New Logic)
-        # Gelatin contains Amino acids (Glycine, Proline) which have Amine groups.
-        # Amine groups react with CO2 (Chemisorption).
+
         gelatin_ratio = recipe.get("GELATIN", 0)
-        amine_efficiency_factor = 15.0 # High affinity constant
+        amine_efficiency_factor = 15.0 
         gelatin_boost_mg_g = gelatin_ratio * 100 * amine_efficiency_factor
         
-        # 4. Final Capacity Calculation
         micropore_efficiency = 1.0 + (quantum_score * 0.6)
         
-        # Biochar Contribution
         biochar_capacity_mg_g = (generated_surface_area * 0.035) + \
                                 (generated_surface_area * chem_factor * ACTIVATION_PHYSICS["SURFACE_CHEMISTRY_FACTOR"])
         biochar_capacity_mg_g *= micropore_efficiency
 
-        # Composite Total = (Biochar Contribution) + (Gelatin Amine Contribution)
         mass_biochar = recipe.get("BIOCHAR", 0) * 100
-        mass_matrix_remainder = recipe.get("STARCH", 0) * 100 # Starch is neutral/blocking
+        mass_matrix_remainder = recipe.get("STARCH", 0) * 100 
         
-        # Starch blocks some pores, but Gelatin helps now
         starch_penalty = 1.0 - (recipe.get("STARCH", 0) * 0.2)
         
         total_uptake_mg = (mass_biochar * biochar_capacity_mg_g * starch_penalty) + \
-                          (gelatin_boost_mg_g) # Adding the matrix boost directly
+                          (gelatin_boost_mg_g) 
                           
         total_uptake_g = total_uptake_mg / 1000.0
         
@@ -78,7 +70,6 @@ class ChemicalValidator:
         
         is_spontaneous = delta_G < self.max_gibbs
         
-        # Updated score metric
         score = predicted_grams * 250 
         
         return {
@@ -90,4 +81,5 @@ class ChemicalValidator:
                 "Predicted_CO2_Grams": predicted_grams,
                 "Virtual_Surface_Area_m2g": surface_area
             }
+
         }
